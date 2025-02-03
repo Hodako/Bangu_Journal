87dacaf2372490c2841aab9715f7cde73407bf3f
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-03 16:35:51 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,7 @@ import Upload from './pages/Upload';
 import Article from './pages/Article';
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [articles, setArticles] = useState([]);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const res = await axios.get(`${process.env.REACT_APP_API_URL}/articles`);
-      setArticles(res.data);
-    };
-
-    fetchArticles();
-  }, []);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -30,15 +19,13 @@ const App = () => {
 
   return (
     <Router>
-      <div className={`app ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-        <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} isAuthenticated={isAuthenticated} />
-        <Routes>
-          <Route path="/" element={<Home articles={articles} isDarkMode={isDarkMode} />} />
-          <Route path="/auth" element={<AuthPages />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/article/:id" element={<Article />} />
-        </Routes>
-      </div>
+      <Navbar isAuthenticated={isAuthenticated} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthPages />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/article/:id" element={<Article />} />
+      </Routes>
     </Router>
   );
 };
