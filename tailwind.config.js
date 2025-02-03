0106c7e626a3600b478260ea9e879e1bd4dd4b55
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-03 11:00:27 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
   ],
-  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
